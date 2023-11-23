83c3a39e95fb3e1c83ece346d4c261304636318d
kaylinkhanal10 <kaylin.k@foundationai.com>
2023-11-23 09:08:18 +0545

    added counter route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import './App.css';
 import Home from "./containers/home";
 import Box from './features/box/box';
+import Counter from './features/Counter/counter'
 import { Route, Routes } from "react-router-dom"
 function App() {
   return (
     <div className="App">
       <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/counter" element={<Counter />} />
+
       <Route path="/box" element={<Box />} />
     </Routes>
     </div>
